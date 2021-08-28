58f3ab85119ad9acd9551bd5cb7ffb53364fec73
ISOLOI <nicholasjkaufmann@gmail.com>
2021-08-28 01:51:24 -0400

    initial

---
+++ src/App.js
@@ -0,0 +1,36 @@
+import PageWrapper from "./components/PageWrapper";
+import {BrowserRouter as Router, Route, Link} from 'react-router-dom';
+
+//Pages
+import Home from "./components/Pages/Home";
+import About from "./components/Pages/About";
+import Contact from "./components/Pages/Contact";
+
+function App() {
+  return (
+    <div className="App">
+       <Router>
+          <PageWrapper>
+              <Route
+                  exact={true}
+                  path="/"
+                  component={Home}
+              />
+
+              <Route
+                  path="/about"
+                  component={About}
+              />
+
+              <Route
+              path="/contact"
+              component={Contact}
+              />
+              
+          </PageWrapper>
+       </Router>
+    </div>
+  );
+}
+
+export default App;
